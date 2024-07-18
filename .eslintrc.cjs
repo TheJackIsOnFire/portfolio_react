57cd79670660fe72d90b75626c061f97469618d7
The Old Jack <daymonblackgamer@gmail.com>
2024-07-18 17:10:08 -0300

    Construindo a estrutura

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import styled, { createGlobalStyle } from 'styled-components'
 
 const EstiloGlobal = createGlobalStyle`
   *{
@@ -6,5 +6,23 @@ const EstiloGlobal = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  body {
+    padding-top: 80px;
+  }
 `
+
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 128px auto;
+  column-gap: 56px;
+
+  img {
+    max-width: 100%;
+  }
+`
+
 export default EstiloGlobal
